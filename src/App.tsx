797dd8b5e782b7a62a2632c3f2b1e8e0e6a9016a
damilare <opeyemisamuel222@gmail.com>
2021-06-02 10:24:14 +0100

    initializing esbuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as esbuild from "esbuild-wasm";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = () =>{
-      console.log(input);
-  }
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    console.log(input);
+  };
   return (
     <div>
       <textarea
