4c9b7aef2f7253e25947df464ea959135823276b
damilare <opeyemisamuel222@gmail.com>
2021-06-06 11:49:49 +0100

    advance configuration of editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor language='javascript' height='500px' />;
+  return (
+    <MonacoEditor
+      theme='dark'
+      language='javascript'
+      height='500px'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
+      }}
+    />
+  );
 };
 export default CodeEditor;
