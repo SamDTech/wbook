55707210564eb1fbda482754c01865b5cdbc4a9d
damilare <opeyemisamuel222@gmail.com>
2021-06-06 18:20:47 +0100

    adding prettier

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
 
 interface CodeEditorProps {
   initialValue: string;
@@ -7,6 +9,10 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onFormatClick =() =>{
+    // get  
+  }
+
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
@@ -15,23 +21,26 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     monacoEditor.getModel()?.updateOptions({tabSize: 2})
   };
   return (
-    <MonacoEditor
-      value={initialValue}
-      editorDidMount={onEditorDidMount}
-      theme='dark'
-      language='javascript'
-      height='500px'
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        value={initialValue}
+        editorDidMount={onEditorDidMount}
+        theme='dark'
+        language='javascript'
+        height='500px'
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
   );
 };
 export default CodeEditor;
