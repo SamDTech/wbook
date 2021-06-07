fdcb30ba1bfd898e705ec53691876aefe90bd90c
damilare <opeyemisamuel222@gmail.com>
2021-06-07 11:30:01 +0100

    adding library

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -39,7 +39,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
   };
   return (
     <div>
-      <button onClick={onFormatClick}>Format</button>
+      <button className='button button-format is-primary is-small' onClick={onFormatClick}>Format</button>
       <MonacoEditor
         value={initialValue}
         editorDidMount={onEditorDidMount}
